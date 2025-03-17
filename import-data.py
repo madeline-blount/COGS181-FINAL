ba8030489e7a0da88620aef37a6560723f3bbbda
Madeline Blount <mgblount@ucsd.edu>
2025-03-17 16:14:57 -0700

    Add files via upload
    filtered for babbling audio

--- import-data.py
+++ import-data.py
@@ -30,10 +30,22 @@ df['YTID'] = df['YTID'].str.strip().str.lstrip('-')
 # Generate YouTube URLs
 df['youtube_url'] = "https://www.youtube.com/watch?v=" + df['YTID']
 
+# df.to_csv("cleaned_train_segments.csv", index=False)
+
+# Define the babbling label(s) you want to filter
+babbling_labels = ['/m/0261r1']
+# Filter rows where 'positive_labels' contains any of the babbling labels
+df_babbling = df[df['positive_labels'].apply(lambda x: any(label in x for label in babbling_labels))]
+
 # Verify output
-print(df[['YTID', 'youtube_url']].head())
+print(df_babbling[['YTID', 'youtube_url']].head())
 
-df.to_csv("cleaned_train_segments.csv", index=False)
+# Save the filtered data to a new CSV
+df_babbling.to_csv("cleaned_babbling_train_segments.csv", index=False)
 
-print(df.head())
+# Set pandas display options to show all columns
+pd.set_option('display.max_columns', None)  # Show all columns
+
+# Check the filtered data
+print(df_babbling.head())
 
